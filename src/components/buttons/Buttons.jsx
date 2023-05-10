31d3f2dc94204b425a6601fbb635571cdaa8b99a
mrlrcn <m.alarconarbona@gmail.com>
2023-05-10 20:50:24 +0200

    added buttons

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,7 +1,16 @@
+import './Buttons.scss';
+
 export default function Buttons() {
     return (
         <>
-        <button>Boton</button>
+
+        <button className="btn btn-primary">Button Primary</button>
+        <button className="btn btn-secondary">Button Secondary</button>
+        <button className="btn btn-cta">Button CTA</button>
+        <a href="" className="btn btn-primary">Link as btn primary</a>
+        <a href="" className="btn btn-secondary">Link as button secondary</a>
+        <a href="" className="btn btn-cta">Link as button cta</a>
+        
         </>
     )
 }
--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -8,24 +8,51 @@ export default function Styleguide() {
         <>
 
         <Nav />
-        <p>Styleguide: Muestrario.</p>
+        <p>Project Gamma: Styleguide</p>
 
         <section className="styleguideSection styleguideSection__headers">
             
+            <h3>Text & Fonts</h3>
+            <div className="line-divider"></div>
             <h1>Esto es un H1</h1>
             <h2>Esto es un H2</h2>
             <h3>Esto es un H3</h3>
             <h4>Esto es un H4</h4>
             <h5>Esto es un H5</h5>
             <h6>Esto es un H6</h6>
-
+            <p>Esto es un parrafo donde pone cosas y textos y negrita y esas cosas parrafo parrafo blabla bla.</p>
         </section>
+
+        <section className="styleguideSection-colors">
+
+            <section className="styleguideSection styleguideSection-colors__colors">
+                <h3>Colors</h3>
+                <div className="line-divider"></div>
+                <div className="color-palette color1"></div>
+                <div className="color-palette color2"></div>
+                <div className="color-palette color3"></div>
+                <div className="color-palette color4"></div>
+
+                
+            </section>
+        
+            <section className="styleguideSection styleguideSection-colors__grayscale">
+                <h3>Grayscale</h3>
+                <div className="line-divider"></div>
+                    <div className="color-palette colorw"></div>
+                    <div className="color-palette colorg"></div>
+                    <div className="color-palette colorgd"></div>
+                    <div className="color-palette colorb"></div>
+            </section>
+
 
         <section className="styleguideSection styleguideSection__buttons">
 
-            <Buttons></Buttons>
+            <Buttons />
 
         </section>
+
+         </section>
         
         </>
     )
